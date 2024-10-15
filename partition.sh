c5956c67d6437e96062ef2cab98e598129350540
monkey <arch@mail.com>
2024-10-15 18:52:29 +0500

    fasdf

--- partition.sh
+++ partition.sh
@@ -17,18 +17,21 @@ echo "Swap partition size: $SWAP_SIZE"
 echo "Root partition size: $ROOT_SIZE"
 echo "Home partition size: $HOME_SIZE"
 
-# Create GPT partition table
-echo -e "o\nn\np\n1\n\n+${SWAP_SIZE}\nn\np\n2\n\n+${ROOT_SIZE}\nn\np\n3\n\n\nw" | fdisk $DISK_DEVICE
+# Create GPT label
+parted -s $DISK_DEVICE mklabel gpt
+
+# Create partitions
+echo -e "mkpart primary fat32 1MiB 2MiB\nmkpart primary linux-swap 2MiB +${SWAP_SIZE}\nmkpart primary ext4 +${SWAP_SIZE} +${ROOT_SIZE}\nmkpart primary ext4 +${ROOT_SIZE} 100%\n" | parted -s $DISK_DEVICE
 
 # Format partitions
-mkswap ${DISK_DEVICE}1
-swapon ${DISK_DEVICE}1
-mkfs.ext4 ${DISK_DEVICE}2
+mkswap ${DISK_DEVICE}2
+swapon ${DISK_DEVICE}2
 mkfs.ext4 ${DISK_DEVICE}3
+mkfs.ext4 ${DISK_DEVICE}4
 
 # Mount partitions
-mount ${DISK_DEVICE}2 /mnt
+mount ${DISK_DEVICE}3 /mnt
 mkdir /mnt/boot
 mount ${DISK_DEVICE}1 /mnt/boot
 mkdir /mnt/home
-mount ${DISK_DEVICE}3 /mnt/home
+mount ${DISK_DEVICE}4 /mnt/home
